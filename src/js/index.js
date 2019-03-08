a967c1dfafcce188658d4988ca5f4c5ff718a888
DEBRENNE XAVIER <xdebrenn@iut.local>
2019-03-08 14:38:23 +0100

    Ajout de l'arborescence scss, mis en place du header

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+document.getElementById("nav__button").addEventListener('click', function(){
+  this.classList.toggle( 'button__toggle' );
+
+});
+
+document.getElementById("nav").addEventListener('click', function(){
+  this.classList.toggle( 'nav__toggle' );
+
+});
